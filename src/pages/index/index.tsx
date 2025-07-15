9855772ce09698419e978c65358fb6b4806c6795
simasong <44399714+lmmqxyx404@users.noreply.github.com>
2025-07-16 00:58:19 +0800

    feat: add nutui react library

--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -1,4 +1,5 @@
 import { View, Text } from '@tarojs/components'
+import { Button } from '@nutui/nutui-react-taro'
 import { useLoad } from '@tarojs/taro'
 import './index.scss'
 
@@ -10,6 +11,7 @@ export default function Index () {
   return (
     <View className='index'>
       <Text>Hello world!</Text>
+      <Button type='primary'>NutUI Button</Button>
     </View>
   )
 }
