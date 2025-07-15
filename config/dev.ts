38c698ccacfe96c88aba5755628ade148acf04cb
lmmqxyx <44399714+lmmqxyx404@users.noreply.github.com>
2025-07-16 03:20:56 +0800

    feat: add the dev server config

--- config/dev.ts
+++ config/dev.ts
@@ -1,7 +1,13 @@
 import type { UserConfigExport } from "@tarojs/cli"
 
 export default {
-  
+
   mini: {},
-  h5: {}
+  h5: {
+    devServer: {
+      port: 18080,
+      host: 'localhost',
+      open: true,
+    }
+  }
 } satisfies UserConfigExport<'vite'>
